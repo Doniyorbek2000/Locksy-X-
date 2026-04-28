38a25ed549ce1e3df0c1d225e50601da5c5230c3
Doniyorbek2000 <victorucbot@gmail.com>
2026-04-28 10:48:53 +0500

    fix: database ssl connection

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -22,9 +22,9 @@ import { UpdatesModule } from './updates/updates.module';
       url: process.env.DATABASE_URL,
       autoLoadEntities: true,
       synchronize: true, // DEV ONLY
-      ssl: process.env.DATABASE_URL?.includes('render.com') || process.env.DATABASE_URL?.includes('neon.tech')
-        ? { rejectUnauthorized: false }
-        : false,
+      ssl: {
+        rejectUnauthorized: false,
+      },
     }),
     ThreatsModule,
     ScanModule,
